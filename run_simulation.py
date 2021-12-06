4e72eb887a2083ff28d93ea1be439f434d399076
aelwood <adam.elwood@lastminute.com>
2021-12-06 10:44:54 +0100

    working with tf

--- run_simulation.py
+++ run_simulation.py
@@ -17,6 +17,11 @@ from policies import (
     LimitedNeuralNetworkRewardEstimator,
 )
 
+import tensorflow as tf
+physical_devices = tf.config.list_physical_devices('GPU')
+for device in physical_devices:
+    tf.config.experimental.set_memory_growth(device, True)
+
 from scipy.stats import uniform
 
 
@@ -568,7 +573,7 @@ if __name__ == "__main__":
 
                 possible_policies.append(
                     MaxEntropyModelFreeContinuousHmc(
-                        mcmc_initial_state=5,
+                        mcmc_initial_state=5.0,
                         name=f'MEMF_HMC_NN_{[str(x) + "_" for x in nn_layers]}_a{str(alpha).replace(".", "")}',
                         alpha_entropy=alpha,
                         reward_estimator=LimitedNeuralNetworkRewardEstimator(
