34b51ff757dcd99a1f5cb446491e15fcd302bc19
mleonardi1 <marco.leonardi@lastminute.com>
2022-08-05 18:06:52 +0200

    plots

--- extra/plot_distributions.py
+++ extra/plot_distributions.py
@@ -1,17 +1,64 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+from sklearn.datasets import make_blobs, make_moons, make_swiss_roll, make_circles
+
+action_offset = 3
+number_of_different_context=2
+fixed_variances=0.6
+n_context_features=3
+environment_best_action_offset=1
+mul_factor = 1
+
+context_reward_parameters = {
+    context_id: {
+        "mu": context_id * action_offset + environment_best_action_offset,
+        "sigma": fixed_variances,
+    }
+    for context_id in range(number_of_different_context)
+}
 
 
-mean = 4
-sd = 0.1
-mul_factor = 0.2
-x_axis = np.arange(1, 8, .01)
-plt.plot(x_axis, norm.pdf(x_axis, mean, sd)*mul_factor)
+x_axis = np.arange(-1, 6, 0.1)
+
+for context_id in range(number_of_different_context):
+    plt.plot(x_axis, norm.pdf(x_axis, context_reward_parameters[context_id]['mu'], context_reward_parameters[context_id]['sigma']))
+plt.title('Probability Density Function of the two reward functions')
+plt.xlabel('Actions')
+plt.ylabel('Probability')
+plt.show()
+
+context_vectors, context_ids = make_blobs(
+            n_samples=10_000,
+            n_features=2,
+            centers=number_of_different_context,
+            cluster_std=0.4,
+            shuffle=True,
+        )
+
+
+for context_id in range(number_of_different_context):
+    plt.scatter(context_vectors[context_ids==context_id, 0], context_vectors[context_ids==context_id, 1])
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title('Linearly separable context distribution')
 plt.show()
 
 
-# for variance,mul in zip([60,50,40,30,20,10],[100,90,90,70,40,20])
+circle_factor=4.
 
-# [0.6,0.5,0.4,0.3,0.2,0.1]
-# [1,1,0.9,0.7,0.4,0.2]
+context_vectors, context_ids = make_circles(
+    n_samples=10_000,
+    shuffle=True,
+)
+context_vectors[context_ids == 0, 0] *= circle_factor
+context_vectors[context_ids == 0, 1] *= circle_factor
+
+
+
+for context_id in range(number_of_different_context):
+    plt.scatter(context_vectors[context_ids==context_id, 0], context_vectors[context_ids==context_id, 1])
+plt.xlabel("x")
+plt.ylabel("y")
+plt.title('Linearly separable context distribution')
+plt.show()
